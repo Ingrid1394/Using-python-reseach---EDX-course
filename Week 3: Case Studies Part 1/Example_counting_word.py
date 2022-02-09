282774b9a90627143526297fb5f96aac1839cedc
Ingrid <94757517+Ingrid1394@users.noreply.github.com>
2022-02-09 15:38:30 -0300

    Update Example_counting_word.py

--- Week 3: Case Studies Part 1/Example_counting_word.py
+++ Week 3: Case Studies Part 1/Example_counting_word.py
@@ -1,2 +1,37 @@
 # Using-python-reseach---EDX-course
 Exercises of Python 
+
+#Example 1
+text= "This is my test text. We' re keeping this text short to keep things manageable"
+
+def count_words(text):
+    text=text.lower()
+    skips=[".",",",";",":","'",'"']
+    for ch in skips:
+        text = text.replace(ch,"")
+   
+    word_counts = {}
+    for word in text.split(" "):
+        if word in word_counts:
+            word_counts[word]+=1
+        else:
+            word_counts[word]=1
+    return word_counts 
+
+
+#Example 2
+from collections import Counter
+def count_words_fast(text):
+    """
+    Count the number of times each word occurs in text (str). Return dictionary 
+    where keys are unique words and values are word counts. Skip punctuation.
+
+    """
+    text=text.lower()
+    skips=[".",",",";",":","'",'"']
+    for ch in skips:
+        text = text.replace(ch,"")
+   
+    word_counts = Counter(text.split(" "))
+    return word_counts 
+    
